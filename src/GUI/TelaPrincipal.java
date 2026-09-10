aafcc0e6d185883effd1bf0089eba0e0f571cdac
pedrohtoff <pedrohtoff@gmail.com>
2026-09-09 22:45:09 -0400

    Criação do Cabeçalho do Hud

--- src/GUI/TelaPrincipal.java
+++ src/GUI/TelaPrincipal.java
@@ -2,13 +2,19 @@ package GUI;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class TelaPrincipal extends Application {
@@ -25,6 +31,8 @@ public class TelaPrincipal extends Application {
         // Scroll para visualizar as mensagens
         ScrollPane scroll = new ScrollPane(mensagens);
         scroll.setFitToWidth(true);
+        mensagens.getChildren().add(new Text("Voce: Olá!"));
+        mensagens.getChildren().add(new Text("Bot: Olá! Como posso ajudá-lo hoje?"));
 
         // Campo para digitar
         TextField campoMensagem = new TextField();
@@ -38,17 +46,29 @@ public class TelaPrincipal extends Application {
         entrada.setPadding(new Insets(10));
         entrada.getChildren().addAll(campoMensagem, enviar);
 
+        // Barra superior
+        HBox superior = new HBox(10);
+        Text titulo = new Text("Banca$h");
+        Text status = new Text("Online");
+        Circle indicadorStatus = new Circle(5, Color.GREEN);
+
+        Region espaco = new Region();
+        HBox.setHgrow(espaco, Priority.ALWAYS);
+
+        superior.getChildren().addAll(titulo, espaco, indicadorStatus, status);
+        superior.setAlignment(Pos.CENTER);
         // Fazendo o textfield ocupar o maximo do espaço
-        HBox.setHgrow(campoMensagem, javafx.scene.layout.Priority.ALWAYS);
+        HBox.setHgrow(campoMensagem, Priority.ALWAYS);
 
         // Montando a tela
+        root.setTop(superior);
         root.setCenter(scroll);
         root.setBottom(entrada);
 
         // Criando a cena
         Scene scene = new Scene(root, 500, 600);
 
-        stage.setTitle("Banca$h");
+        stage.setTitle("App");
         stage.setScene(scene);
         stage.show();
     }
